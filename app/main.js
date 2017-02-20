31d2f94dc7e5743c51eb1f593bc832f352f91128
tplots <25012061@qq.com>
2017-02-20 10:35:55 +0800

    -

---
+++ app/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Publisher from './Hello.jsx';
+
+ReactDOM.render(
+    <Publisher />,
+    document.getElementById('container')
+);
